cde12abf64f38d117e3378dd3c1a595e805dd761
testtest1241 <cryptofaith1+++@gmail.com>
2025-05-25 16:18:06 +0300

    Update Game.tsx

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -24,7 +24,7 @@ const Game = () => {
         physics: {
           default: 'arcade',
           arcade: {
-            gravity: { y: 0 },
+            gravity: { x: 0, y: 0 },
             debug: false
           }
         },
